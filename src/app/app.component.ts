b7668c5bfc7beff6bd5607f7e54795842808a942
reda96 <aboreda9600@gmail.com>
2021-08-10 21:46:48 +0200

    add-recipeItem-content-and-pass-data-from-recipeLit-using-custom-propert-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShoppingListAndRecipeBookApp';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature) {
+    this.loadedFeature = feature;
+  }
 }
