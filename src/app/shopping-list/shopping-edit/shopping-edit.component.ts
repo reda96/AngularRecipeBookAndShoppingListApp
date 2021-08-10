3304007f77a2a069cf206394ef79f609fd202d61
reda96 <aboreda9600@gmail.com>
2021-08-10 23:24:59 +0200

    add-Ingredient-to-shopping-list-work

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @Output() ingredientDeleted = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onAddIngredient() {
+    this.ingredientAdded.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value,
+    });
   }
-
+  onDeleteIngredient() {
+    this.ingredientDeleted.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value,
+    });
+  }
 }
